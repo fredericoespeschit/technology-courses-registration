fd871cf82cb85552ea7e5db210d2517be5ac69ba
fredericoespeschit <fespeschitt@gmail.com>
2023-06-25 14:22:16 -0300

    Lista de Cursos: Tratamento de Erros e MatDialog

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CoursesComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule
   ],
 })
 export class CoursesModule {}
