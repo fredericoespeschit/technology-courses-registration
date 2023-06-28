f5bae901bc28223098e999635959334994cc2677
fredericoespeschit <fespeschitt@gmail.com>
2023-06-28 19:48:13 -0300

    Conectando o Angular na API Spring

--- technology-courses-registration-frontend/src/app/courses/courses/courses.component.ts
+++ technology-courses-registration-frontend/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Course } from '../models/course';
-import { CoursesService } from './../services/courses.service';
-import { Observable, catchError, of } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { Course } from "../models/course";
+import { CoursesService } from "./../services/courses.service";
+import { Observable, catchError, of } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
+import { ErrorDialogComponent } from "src/app/shared/components/error-dialog/error-dialog.component";
 
 @Component({
   selector: "app-courses",
@@ -15,24 +15,24 @@ export class CoursesComponent {
   public courses$: Observable<Course[]>;
   displayedColumns = ["name", "category"];
 
-  constructor(private coursesService: CoursesService,
-    public dialog: MatDialog) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     this.courses$ = this.coursesService.list().pipe(
-      catchError(error => {
+      catchError((error) => {
         // console.log(error)
-        this.onError('Erro ao carregar cursos.')
-        return of([])
+        this.onError("Erro ao carregar cursos.");
+        return of([]);
       })
     );
   }
 
   onError(errorMsg: string) {
     this.dialog.open(ErrorDialogComponent, {
-      data: errorMsg
+      data: errorMsg,
     });
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 }
--- technology-courses-registration-frontend/src/app/courses/services/courses.service.ts
+++ technology-courses-registration-frontend/src/app/courses/services/courses.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course';
-import { delay, take, tap } from 'rxjs';
+import { take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
- private readonly API = '/assets/courses.json';
+ private readonly API = 'api/courses';
   constructor(private httpClient : HttpClient) { }
 
   list() {
